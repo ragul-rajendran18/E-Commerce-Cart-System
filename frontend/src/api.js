aca4978b5642535c4934aa883b6ef23931cdb578
ragul-rajendran18 <ragul18rs@gmail.com>
2025-10-24 22:21:07 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://e-commerce-cart-system-backend.onrender.com' });
+const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 export const fetchProducts = () => API.get('/products').then(r => r.data);
 export const checkout = (items) => API.post('/cart/checkout', { items }).then(r => r.data);
